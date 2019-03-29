f38154667986d96efe6559b2925151531ef93f53
isaacceba <isaac.ceballos408@gmail.com>
2019-03-29 00:28:03 +0000

    TA Help

--- Gemfile
+++ Gemfile
@@ -71,9 +71,7 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
 
-gem 'will_paginate', '>= 3.1'
+gem 'will_paginate', '~> 3.1.0'
 
 gem 'simple_form'
-
-
 
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,41 +1,19 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @link = Place.path(page: params[:page]. per_page:)
   end
 
   def new
     @place= Place.new
   end
 
-  def create
-    Place.create(place_params)
+  def index
+    Place.path(page: params[:page], per_page: 30)
     redirect_to root_path
   end
 
   private
 
-  def place_params
-    params.require(:place).permit(:name, description, :address)
-  end
-
 end
 
-
-def self.search(term, current_page)
-  if term
-    page(current_page).where('name LIKE ?', "%#{term}%").order('id DESC')
-  else
-    page(current_page).order('id DESC') 
-  end
-
-
-
-def self.search(term, page)
-  if term
-    where('name LIKE ?', "%#{term}%").paginate(page: page, per_page: 5).order('id DESC')
-  else
-    paginate(page: page, per_page: 5).order('id DESC') 
-  end
-  
-end
