1cbba25eaf3bbd5c3800fb56c823f03bdacdbeb0
isaacceba <isaac.ceballos408@gmail.com>
2019-04-01 20:27:28 +0000

    comments in database

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :places
+  has_many :comments
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
